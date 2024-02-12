f68399d958fe1d93ee190e2391e1b001d1f086fc
kate8kate <kate.bazire@gmail.com>
2024-02-12 19:40:37 +0100

    lesson_08

---
+++ front_end/lesson_08/code/index.js
@@ -0,0 +1,98 @@
+
+let a = 5;
+let str = "555";
+let bool = true;
+let u = undefined;
+let n = null;
+let sy = Symbol('inter');
+let bi = 1257n;
+
+// console.log(typeof str);
+// console.log(typeof bool);
+
+// let typeofVar = typeof bi;
+// console.log(typeofVar);
+
+//*****  Переопределение типов
+// в строку
+
+// console.log(typeof aa);
+// // let aaStr = String(aa);
+// let aaStr = aa + "5";
+
+// console.log(typeof aaStr);
+// console.log(aaStr);
+
+// в число
+
+// let strNum = Number(str);
+// let strNum = 10 + +"105";
+// let strNum = Number(true); 1
+// let strNum = Number(false); 0
+
+//  console.log(strNum + " = "+ typeof strNum);
+
+// let strNum = Number("h15");
+// console.log (typeof strNum);
+
+// Boolean
+// let newBool = Boolean("mike"); true
+// let newBool = Boolean(1);  true
+// let newBool = Boolean(-1);  true
+// let newBool = Boolean(0);  false
+// let newBool = Boolean(null); false
+// let newBool = Boolean(undefined); false
+// console.log(newBool);
+
+// if (-1){
+//     console.log('true');
+// }else{
+//     console.log('false');
+// }
+
+//  Operators
+// + - / * %  ** (Math)
+
+// if (true){
+//     console.log();
+// } 
+
+// const tREe = (true) ? a : b;
+
+// > < ; >= <= ;
+
+// === не сравнивает разные типы; == переводит Boolean -> Number -> String !!!
+
+// console.log(9 === 9); true
+// console.log(9 === "9"); false
+// console.log(9 == "9"); true
+// console.log(1 == true); true
+// console.log(true == "1"); true
+// console.log(true == "2"); false
+// console.log(true == "true"); false
+
+
+
+// //****
+
+// let newa = 1;
+
+// if (true) {
+//     newa = 9;
+//     console.log(newa);
+// }
+// console.log(newa);
+// //****
+
+// //****  scope область видимости
+
+// newa = 1;
+// console.log(newa);
+
+// if (true) {
+//    newa = 9;
+//     console.log(newa);
+// }
+// console.log(newa);
+// //****
+
---
+++ front_end/lesson_08/hw_08/code/index2.js
@@ -0,0 +1,34 @@
+const a = 17;
+const b = 5;
+console.log((a > b));
+console.log((a <= b));
+console.log((a == b));
+console.log((a != b));
+
+console.log('a' < 'b');
+console.log('ab' > 'a');
+
+
+console.log('17' > 1);
+
+console.log(17 === 1);
+console.log(17 === 17);
+console.log(17 === '17');
+console.log(17 === true);
+console.log('0' === '');
+console.log(true === false);
+console.log(true === true);
+console.log(null === undefined);
+console.log(false === 0);
+
+console.log(17 == '17');
+console.log('0' == '');
+console.log(0 == '');
+console.log(null == undefined);
+console.log(false == 0);
+
+console.log(undefined == null);
+console.log(undefined == 0);
+console.log(null == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
