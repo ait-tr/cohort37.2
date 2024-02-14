fcdceaf6f39811bb63472b67f9e3fc0c94137675
Michelle <153286968+MischkaPCHI@users.noreply.github.com>
2024-02-14 14:05:40 +0100

    исправление в for  i не 2 а 3

--- front_end/lesson_10/homework/hw_4/index.js
+++ front_end/lesson_10/homework/hw_4/index.js
@@ -19,7 +19,7 @@ function find(num2){
 
     const nums = [];
 
-    for(let i = 2; i <= num2; i++){
+    for(let i = 3; i <= num2; i++){         // 1,2 у нас точно не являются простыми
         if(boolean(i)){
            nums.push(i);
         }
