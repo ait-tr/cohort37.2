0b54a84515254134d93da2b16143c5d24c053a5d
Katerina Selem <katerinaselem@gmail.com>
2024-02-16 00:28:52 +0100

    newchanges

--- front_end/lesson_11/code/scripts/index.js
+++ front_end/lesson_11/code/scripts/index.js
@@ -1,11 +1,21 @@
 
 
-// console.log(secondItemElem);
-// console.log(secondItemElem.classList);
-
 const secondItemElem = document.querySelector('#second-item');
 
-const secondelementClick = () => {
-    secondItemElem.classList.add('list-item-done');
+
+// console.log(secondItemElem);
+
+
+
+// console.log(document);
+
+
+
+// console.log(secondItemElem.innerHTML);
+
+const secondItemElemClick = () => {
+    secondItemElem.classList.toggle('list-item_done'); //add / remove / toggle
+    // secondItemElem.style.backgroundColor = 'blue';
 }
-secondItemElem.addEventListener('clic', secondelementClick);
+
+secondItemElem.addEventListener('click', secondItemElemClick);
