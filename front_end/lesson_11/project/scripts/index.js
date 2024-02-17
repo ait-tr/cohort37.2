a48703c53151cad8fcf5d2496f5156fbe4062d54
Katerina Selem <katerinaselem@gmail.com>
2024-02-17 16:14:30 +0100

    new version of progect

--- front_end/lesson_11/project/scripts/index.js
+++ front_end/lesson_11/project/scripts/index.js
@@ -1,37 +1,54 @@
 const inputHeightElement = document.querySelector('#heightInput');
+const inputWeightElement = document.querySelector('#weighInput');
+const calculateButtonElemnt = document.querySelector('.button');
+const bmiOutput = document.querySelector('#result1');
+const resultOutput = document.querySelector('#result2');
 
 let userHeightInput;
+let userWeightInput;
 
-const inputHeightFieldInput = (evt)=> {
-    userHeightInput = Number(evt.target.value);
-    console.log(userHeightInput);
+
+
+const heightFieldInput = (evt) => {
+    if (notANumber) {
+        alert("Numbers only");
+    }
+    userHeightInput = evt.target.value;
 }
 
-inputHeightElement.addEventListener('input', inputHeightFieldInput)
-
-const inputWeightElement = document.querySelector('#weighInput');
-
-let userInputWeight =1;
-
-const inputWeightFieldInput = (evt)=> {
-    userInputWeight = Number(evt.target.value);
-    console.log(userInputWeight);
+const weightFieldInput = (evt) => {
+    if (notANumber) {
+        alert("Numbers only");
+    }
+    userWeightInput = evt.target.value;
 }
-
-inputWeightElement.addEventListener('input', inputWeightFieldInput);
-
-const calculateButtonElemnt = document.querySelector('.button');
-
-//console.log(calculateButtonElemnt);
-
-const bmiOutput = document.querySelector('#result1');
 
 const submitButtonHandler = () => {
-    console.log(userHeightInput);
-    const result = Math.ceil(userInputWeight/(userHeightInput**2)); 
-    bmiOutput.textContent = result;
+    let bmi = Math.round(userWeightInput / ((userHeightInput / 100) ** 2));
+    bmiOutput.textContent = bmi;
+    let result;
+    if (bmi > 18 && bmi < 25) {
+        result = "Okay";
+    }
+    if (bmi < 18) {
+        result = "Underweight ";
+    }
+    if (result > 25) {
+        result = "Overweight";
+    }
+    resultOutput.textContent = result;
 }
+
+const notANumber = () => {
+    if (value === NaN) {
+        return true;
+    } else { 
+        return false;
+    }
+}
+
+
+
+inputHeightElement.addEventListener('input', heightFieldInput);
+inputWeightElement.addEventListener('input', weightFieldInput);
 calculateButtonElemnt.addEventListener('click', submitButtonHandler);
-
-
-
