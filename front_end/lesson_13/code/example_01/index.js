5dbd39422b35fc3af3a2f64509b8b17597f67f0e
Vitaliy Vynnyk <Vitaliy.vinnik@gmail.com>
2024-02-20 09:00:03 +0100

    adds lesson_13 code example

---
+++ front_end/lesson_13/code/example_01/index.js
@@ -0,0 +1,40 @@
+// const result = (from, to, amount) => {
+//     let k1;
+//     let k2;
+//     if (from === "USD") {
+//         k1 = 1;
+//     };
+//     if (from === "EUR") {
+//         k1 = 0.92;
+//     };
+//     if (from === "GBP") {
+//         k1 = 0.8
+//     };
+
+//     if (from === "USD") {
+//         k2 = 1;
+//     };
+//     if (from === "EUR") {
+//         k2 = 0.92;
+//     };
+//     if (from === "GBP") {
+//         k2 = 0.8
+//     };
+//     const sum = amount/k1*k2;
+//     return sum;
+// } 
+
+
+// result('EUR', 'GBP', 100);
+
+
+
+
+const result = (a , b) => {
+    const sum = a + b;
+    return 77;
+} 
+
+console.log((result(5, 4));
+
+
