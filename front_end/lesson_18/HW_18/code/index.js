a7c1c577bbaa2197122be8f296ed93955dd1b134
kate8kate <kate.bazire@gmail.com>
2024-02-26 17:20:43 +0100

    kate_gonshteyn

--- front_end/lesson_18/HW_18/code/index.js
+++ front_end/lesson_18/HW_18/code/index.js
@@ -21,19 +21,30 @@ console.log(sum(nums));
 // Task3: Написать функцию, которая принимает на вход массив чисел и возвращает 
 // массив из тех же элементов, но в обратном порядке.
 
+const numbers = [3, 7, 9, 1, 6, 10];
+
+function revNumbers(numbers) {
+    return numbers.reverse();
+}
+console.log(revNumbers(numbers));
 
 // Task4: Написать функцию, которая примет объект вида:
 
-// {
-//    'customer-id-1': {
-//       name: 'William',
-//       age: 54
-//    },
-//    'customer-id-2': {
-//       name: 'Tоm',
-//       age: 17
-//    }
-// }
+const customer = {
+    'customer-id-1': {
+       name: 'William',
+       age: 54
+    },
+    'customer-id-2': {
+       name: 'Tоm',
+      age: 17
+   }
+ }
 // и вернет массив обьектов вида:
-
 // [{name: 'William', age: 54, id: 'customer-id-1’}, {name: 'Tom', age: 17, id: 'customer-id-2'}]
+
+const objToArray = (obj) => Object.keys(obj).map((key) => ({
+    id: key,
+    ...obj[key]
+}));
+console.log(objToArray(customer));
