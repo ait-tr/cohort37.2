07892835cc94861087d9238e7bb0c71544f31b0d
KaterinaSelem <katerinaselem@gmail.com>
2024-02-28 20:55:43 +0100

    new version

--- front_end/lesson_20/HomeWork_20/index.js
+++ front_end/lesson_20/HomeWork_20/index.js
@@ -1,5 +1,5 @@
 const userInput = document.querySelector('#incertTime');
-const submitButton = document.querySelector('.button');
+const submitButton = document.querySelector('button');
 const timeCountdown = document.querySelector('#outputTime');
 
 const inputTime = () => {
@@ -8,20 +8,29 @@ if (userInput.value < 0) userInput.value = 0;
 
 const submitButtonHandler = () => {
 
-    let timeRemaining = parseInt(userInput.value);
+    submitButton.removeEventListener('click', submitButtonHandler);
 
+    let timeRemaining = parseInt(userInput.value);
+    
+    
+   
     timeCountdown.textContent = `Left: ${userInput.value}`;
 
-    countdownInterval = setInterval(() => {
+const countdownInterval = setInterval(() => {
         timeRemaining--;
 
         if (timeRemaining <= 0) {
+            clearInterval(countdownInterval);
             timeCountdown.textContent = 'Time is up!';
+            userInput.value = '';
+            submitButton.addEventListener('click', submitButtonHandler);
         } else {
-            timeCountdown.textContent = `Left: ${--userInput.value}`;
+            timeCountdown.textContent = `Left: ${timeRemaining}`;
+             
         }
     }, 1000);
 }
+ 
 
 
 userInput.addEventListener('input', inputTime);
