dd232e4a709f5182f84e73d73bafef1ea526a581
kate8kate <kate.bazire@gmail.com>
2024-03-03 19:42:05 +0100

    kate_gonshteyn

---
+++ front_end/lesson_22/HW_22/index.js
@@ -0,0 +1,22 @@
+// // создаем функцию которая нам вернет промис с результатом запроса по адресу 'q'
+// const todoList = ('https://jsonplaceholder.typicode.com/todos') => {
+//     return fetchTodoList(q)
+// }
+
+function fetchTodoList() {
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response => response.json()) // вернет промис с результатом вызова его функции-аргемента(объект)
+.then(todos => {
+    const listElement = document.querySelector('#todoList');
+    todos.forEach(todo => {
+        const todoItem = document.createElement('li');
+        todoItem.textContent = todo.title; 
+        if (todo.completed) {    
+            todoItem.style.textDecoration = 'line-through';
+        }
+        listElement.append(todoItem);
+        console.log(todos);
+    });
+})
+}
+fetchTodoList();
---
+++ front_end/lesson_22/code/example_01/index.js
@@ -0,0 +1,50 @@
+// создаем функцию которая нам вернет промис с результатом запроса по адресу 'q'
+const getGithubData = (q) => {
+    return fetch(q)
+}
+
+const githubDataKate = getGithubData('https://api.github.com/users/kate8kate');
+
+// console.log(githubDataKate);
+
+githubDataKate
+    .then((response) => response.json()) // then в этой строке вернет промис с результатом вызова его функции-аргемента(объект)
+    .then((resultObject) => {
+        console.log(resultObject);
+        showAvatar(resultObject.avatar_url);
+        showName(resultObject.name);
+        showRepos(resultObject.repos_url);
+
+    return resultObject.reposUrl;
+    })
+    .then((repos) => {
+        console.log(repos);
+        const githubDataKateRepos = getGithubData(repos);
+        githubDataKateRepos
+        .then((response) => {returnresponse.json()})
+        .then((resultObject) => {
+            console.log(resultObject(0).name)
+        })
+        
+    })
+
+
+  
+function showAvatar(avatarUrl) {
+    const containerForAva = document.querySelector('#ava');
+    const avaElement = document.createElement('img');
+    avaElement.src = avatarUrl;
+    containerForAva.append(avaElement);
+}   
+function showName(name) {
+    const containerForName = document.querySelector('#name');
+    containerForName.textContent = name;  
+}
+function showRepos(reposUrl) {
+    const containerForRepos = document.querySelector('#repos');
+    const reposElement = document.createElement('repos');
+    reposElement.src = reposUrl;
+    containerForRepos.append(reposElement);
+}
+
+
