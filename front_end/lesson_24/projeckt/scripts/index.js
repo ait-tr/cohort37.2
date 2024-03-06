55404f7e530c2a5b2fc2a26ce721afc3b09f34ef
Katerina Selem <katerinaselem@gmail.com>
2024-03-06 19:12:33 +0100

    last chages

--- front_end/lesson_24/projeckt/scripts/index.js
+++ front_end/lesson_24/projeckt/scripts/index.js
@@ -12,23 +12,25 @@
 const movieData = async (q, f) => {
     const serverReponse = await fetch(q, f);
     const body = await serverReponse.json(); 
-   // insertDataOfMovie(body);
    const arrayOfFilms = body.films;
-   console.log('arr '+ arrayOfFilms[0].filmId);
+   //console.log('arr '+ arrayOfFilms[0].filmId);
    const dataFilmsWithDescr = await body.films.forEach(async (element, index)=>{
     const dataMovieEl = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${element.filmId}`, f);
     const descrptBody = await dataMovieEl.json();
-    arrayOfFilms[index] = {...arrayOfFilms[index], description: descrptBody.description}
-    console.log(arrayOfFilms[index]);
+   arrayOfFilms[index] = {...arrayOfFilms[index], description: descrptBody.description}
+    console.log(arrayOfFilms[index].nameRu);
+    document.querySelector('main').append(createPoster(arrayOfFilms[index]));
+    console.log(createPoster(arrayOfFilms[index]));
 
     
     
    })
- console.log(body.films);
- console.log(dataFilmsWithDescr);   
+
+
+console.log(arrayOfFilms[0]);   
 }
 const dataOfMovie = movieData('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1', fetchParams );
-console.log(dataOfMovie);
+
 
 
 
@@ -38,6 +40,48 @@ const poster = document.querySelector('.background');
 const movieTitle = document.querySelector('#title');
 const movieTieser = document.querySelector('#teaser');
 const genreElement = document.querySelector('#genre');
+
+
+const createPoster = (element) =>{
+
+
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('item');
+    const dataValueBckImg = element.posterUrl; 
+    mainContainer.style.backgroundImage = `url(${dataValueBckImg})`;
+
+    const containerTitle = document.createElement('div');
+    containerTitle.classList.add('title');
+    containerTitle.textContent = element.nameRu;
+
+    const containerYear = document.createElement('div');
+    containerYear.classList.add('year');
+    containerYear.textContent = element.year;
+
+    const containerTeaser = document.createElement('div');
+    containerTeaser.classList.add('teaser');
+    containerTeaser.textContent = element.description;
+    
+    const wrapContainer = document.createElement('div');
+    wrapContainer.classList.add('wrap');
+    wrapContainer.appendChild(containerTitle);
+    wrapContainer.appendChild(containerYear);
+    wrapContainer.appendChild(containerTeaser);
+
+
+    mainContainer.appendChild(wrapContainer);
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+    container.appendChild(mainContainer);
+
+    return container;
+   
+
+}
+
+
+
 
 // const insertDataOfMovie = (elem) =>{
 //     const values = Object.values(elem);
@@ -50,13 +94,13 @@ const genreElement = document.querySelector('#genre');
 
 //     })
 // }
-const insertDataOfMovie = (elem) =>{
-    const values = Object.values(elem);
-    values.forEach((object) => {
-        insertMovie(object); 
+// const insertDataOfMovie = (elem) =>{
+//     const values = Object.values(elem);
+//     values.forEach((object) => {
+//         insertMovie(object); 
        
-    })
-}
+//     })
+// }
 
 
 
@@ -72,6 +116,11 @@ const insertDataOfMovie = (elem) =>{
 //     const movieYearData = document.createElement('div');
 //     movieYearData.textContent = dataValueYear;
 //     movieYear.append(movieYearData);
+
+//     const dataMovieDescript = object.description; 
+//     const  descriptionData= document.createElement('div');
+//     descriptionData.textContent = dataMovieDescript;
+//     movieTieser.append(movieYearData);
 
 //     const dataValueBckImg = object.posterUrl; 
 //     poster.style.backgroundImage = `url(${dataValueBckImg})`;
